bde782c0add2a0a7b719f97e5bf6c54795b079a6
foxxxion <60135982+foxxxion@users.noreply.github.com>
2020-08-13 09:28:41 +0300

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -i -s -k -X $'GET' \
     -H $'Host: 172.17.0.2' -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H $'Accept-Language: en-US,en;q=0.5' -H $'Accept-Encoding: gzip, deflate' -H $'Referer: https://172.17.0.2/index.html?username=FLAG%7B%7D&password=Elementary_my_dear_Watson' -H $'Connection: close' -H $'Cookie: heartbleed' -H $'Upgrade-Insecure-Requests: 1' \
     $'https://172.17.0.2/index.html?username=FLAG%7B%7D&password=Elementary_my_dear_Watson'
